9b031d4b67c5f1ea6dc2a2e1dd382fde3a43054a
michael <afmkt1020@proton.me>
2024-11-12 12:36:54 +0800

    convert cvat dataset to coco format, and validate generated coco annotations by visulization

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 def train(cvatAnn: str = os.path.join('data', 'lowerboneandimplant', 'annotations.xml')):
     train_path = c.cocoAnnFileName(cvatAnn)
-    model = YOLO('yolo11x-seg.yaml').load('yolo11x-set.pt')
+    model = YOLO('yolo11x-seg.yaml').load('yolo11x-seg.pt')
     start = time.time()
     model.train(data = train_path, epochs = 100, imgsz = 640)
     train_end = time.time()
